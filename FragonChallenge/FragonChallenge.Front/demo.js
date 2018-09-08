de321d39c19eced4d350cf9949423b9028d3c05f
Luiz Fernando Santos <luiz.nascimento@linx.com.br>
2018-09-08 16:27:54 -0300

    -Creating front project and adding AngularJs, AngularJS.route e bootstrap.

---
+++ FragonChallenge/FragonChallenge.Front/demo.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="scripts/angular.js" />
+
+var MyApp = angular.module("MyApp", ['ngRoute']);
+
+MyApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+            when('/AddCustomers', {
+                templateUrl: 'Views/AddCustomers.html',
+                controller: 'AddCustomersController'
+            }).
+            when('/ListCustomers', {
+                templateUrl: 'Views/ListCustomers.html',
+                controller: 'ListCustomersController'
+            }).
+            when('/Home', {
+                templateUrl: 'Views/Home.html',
+                controller: 'HomeController'
+            }).
+            otherwise({
+                redirectTo: '/Home'
+            });
+    }
+]);
+
+MyApp.controller("AddCustomersController", function ($scope) {
+    $scope.message = "Add Customers";
+});
+MyApp.controller("ListCustomersController", function ($scope) {
+    $scope.message = "List Customers";
+});
+MyApp.controller("HomeController", function ($scope) {
+    $scope.message = "Home Customers";
+});
